7580388d1077dde20e52a292e18a924933768bf5
Arzola <JohnCarlo.Arzola@tyson.com>
2022-04-21 21:11:16 -0500

    DOM manipulation
    Practiced DOM manipulation by doing some document.querySelector() codes.

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number!👏";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 20;
